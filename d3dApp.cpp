a45b4506e3a4980eb5c0bc3ee53403d7668811e8
애리 <30598446+Arialari@users.noreply.github.com>
2020-07-14 17:48:07 +0900

    다른 헤더를 가져옴
    프레임워크에 일부를 가져오고
    d3dApp.cpp를 직접 작성하면서 복습
    
    필요시 다른 cpp를 작성하면서 복습할것

---
+++ d3dApp.cpp
@@ -0,0 +1,15 @@
+﻿// 
+//
+
+#include "d3dApp.h"
+#include <WindowsX.h>
+
+using Microsoft::WRL::ComPtr;
+using namespace std;
+using namespace DirectX;
+
+LRESULT CALLBACK
+MainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	return D3DApp::GetApp()->MsgProc(hwnd, msg, wParam, lParam);
+}
