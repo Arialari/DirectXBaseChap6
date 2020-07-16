82b076e9848e910b5681dd47aa05ce9095ae92d9
애리 <30598446+Arialari@users.noreply.github.com>
2020-07-16 16:33:29 +0900

    D3D 생성,소멸자, setter getter, Inits 한개

--- d3dApp.cpp
+++ d3dApp.cpp
@@ -12,4 +12,97 @@ LRESULT CALLBACK
 MainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	return D3DApp::GetApp()->MsgProc(hwnd, msg, wParam, lParam);
+
+	
+}
+
+D3DApp* D3DApp::mApp = nullptr; // QQ : 왜 nullptr로 초기화 하고 사용하는가 분명 생성자에서 mApp=this 했는데.
+D3DApp* D3DApp::GetApp()
+{
+	return mApp;
+}
+
+D3DApp::D3DApp(HINSTANCE hInstance)
+: mhAppInst(hInstance)
+{
+	assert(mApp == nullptr);
+	mApp = this;
+}
+
+D3DApp::~D3DApp()
+{
+	if (md3dDevice != nullptr)
+		FlushCommandQueue();
+}
+
+HINSTANCE D3DApp::AppInst()const
+{
+	return mhAppInst;
+}
+
+HWND D3DApp::MainWnd()const
+{
+	return mhMainWnd;
+}
+
+float D3DApp::AspectRatio()const
+{
+	return static_cast<float>(mClientWidth) / mClientHeight;
+}
+
+bool D3DApp::Get4xMsaaState()const
+{
+	return m4xMsaaState;
+}
+
+void D3DApp::Set4xMsaaState(bool value)
+{
+	if (m4xMsaaState != value)
+	{
+		m4xMsaaState = value;
+		CreateSwapChain();
+		OnResize();
+	}
+}
+
+int D3DApp::Run()
+{
+	MSG msg = { 0 };
+	mTimer.Reset();
+
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			mTimer.Tick();
+			if (!mAppPaused)
+			{
+				CalculateFrameStats();
+				Update(mTimer);
+				Draw(mTimer);
+			}
+			else
+			{
+				Sleep(100);
+			}
+		}
+	}
+
+	return (int)msg.wParam;
+}
+
+bool D3DApp::Initialize()
+{
+	if (!InitMainWindow())
+		return false;
+	if (!InitDirect3D())
+		return false;
+	OnResize();
+
+	return true;
 }
